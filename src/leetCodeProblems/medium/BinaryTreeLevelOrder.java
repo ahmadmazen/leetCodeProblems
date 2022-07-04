4cd20445cd9aeb62161ff59ace3483bcf2718a61
Ahmed R. Mazen <ahmed.mazen87@yahoo.com>
2022-07-04 12:14:02 +0300

    Update BinaryTreeLevelOrder.java
    the definition of TreeNode Data structure

--- src/leetCodeProblems/medium/BinaryTreeLevelOrder.java
+++ src/leetCodeProblems/medium/BinaryTreeLevelOrder.java
@@ -8,6 +8,22 @@ import java.util.Queue;
 import common.CommonUtils;
 import common.TreeNode;
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 public class BinaryTreeLevelOrder {
 
 	/* solved it right one shot using BFS with queue
